5875b20d4bc80b583dd2526683a04be753fb9540
gerard-lorente-lopez <lorentelopezgerard@gmail.com>
2026-01-12 17:49:31 +0100

    instalar dependecias del proyecto, crear archivos en blanco para estructurar el proyecto 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from .routers.viatger import router as viatger_router
 from .routers.creador import router as creador_router
-from .routers.administrador import router as administrador_router
+from .routers.admin import router as admin_router
 
 app = FastAPI(title="Bienvenido a nuestra agencia de viajes")
 
@@ -14,7 +14,7 @@ def read_root():
 # Incluir rutas
 app.include_router(viatger_router)
 app.include_router(creador_router)
-app.include_router(administrador_router)
+app.include_router(admin_router)
 
 
 # Lista de orígenes permitidos
