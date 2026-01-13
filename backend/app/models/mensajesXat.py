519ca7e953a7a01edfd75f22ed7a7cf1f5be9854
gerard-lorente-lopez <lorentelopezgerard@gmail.com>
2026-01-13 17:03:27 +0100

    modelo de mensajes finalizado

--- backend/app/models/mensajesXat.py
+++ backend/app/models/mensajesXat.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Enum as SQLEnum, Text, ForeignKey
+from sqlalchemy import Column, Integer, Enum as SQLEnum, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from ..db.database import Base
 
@@ -7,3 +7,12 @@ class MissatgeXat(Base):
     __tablename__ = "mensajes"
     
     id = Column(Integer, primary_key=True, index=True)
+    viaje_id = Column(Integer, ForeignKey("viajes.id"), nullable=False)
+    autor_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    contenido = Column(Text, nullable=False)
+    timestamp = Column(DateTime, nullable=False)
+
+    # Relación con Viaje
+    viaje = relationship("Viaje", backref="mensajes")
+    # Relación con Usuario
+    autor = relationship("Usuario", backref="mensajes")
