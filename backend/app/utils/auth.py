e889b5656056677e8d464bb65d34e7c3cd7d9c5b
Chivaa8 <adaw23ochiva@inslaferria.cat>
2026-01-20 17:18:00 +0100

    fix: correción y finalización utils/auth.py

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -3,16 +3,14 @@ from passlib.context import CryptContext
 from fastapi import  HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 from typing import Optional
-from sqlalchemy.orm import Session
 from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from ..db.database import SessionLocal
-from ..db.deps import get_db
 from ..models.usuario import Usuario
 import jwt
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/long")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -56,24 +54,3 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-def get_current_user_from_token(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-   
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="No se pudieron validar las credenciales",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except jwt.PyJWTError:
-        raise credentials_exception
-    
-    usuario = db.query(Usuario).filter(Usuario.username == username).first()
-    if usuario is None:
-        raise credentials_exception
-    
-    return usuario
