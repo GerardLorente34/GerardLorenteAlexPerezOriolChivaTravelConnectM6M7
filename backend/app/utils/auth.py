80a6a8d20483f9961c97d1593f1b360952b8cc63
Chivaa8 <adaw23ochiva@inslaferria.cat>
2026-01-20 17:11:47 +0100

    fix: correción y finalización utils/auth.py

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
-from fastapi import Depends, HTTPException, status
+from fastapi import  HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordBearer
 from typing import Optional
 from config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
@@ -10,12 +10,31 @@ import jwt
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/long")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_current_user(username: str):
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if not username:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido", headers={"WWW-Authenticate": "Bearer"})
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido", headers={"WWW-Authenticate": "Bearer"})    
+
+    db = SessionLocal()
+
+    try:
+        user = db.query(Usuario).filter(Usuario.username == username).first()
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")    
+        return user
+    finally:
+        db.close()
+
+def get_user_by_username(username: str):
     db = SessionLocal()
     try:
         return db.query(Usuario).filter(Usuario.username == username).first()
@@ -23,11 +42,11 @@ def get_current_user(username: str):
         db.close()
 
 def authenticate_user(username: str, password: str):
-    usuario = get_current_user(username)
+    usuario = get_user_by_username(username)
     if not usuario:
-        return False
+        return None
     if not verify_password(password, usuario.hashed_password):
-        return False
+        return None
     return usuario
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
