4c9f80e15a211eeb1c2cc387ef47aaf64e1fa51f
gerard-lorente-lopez <lorentelopezgerard@gmail.com>
2026-01-22 16:48:29 +0100

    importar los routers al main y añadir las rutas de destino al frontend 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,8 +33,7 @@ origins = [
     "http://localhost:5173",  # Vite dev
     "http://127.0.0.1:5173",
     "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:8000"
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
